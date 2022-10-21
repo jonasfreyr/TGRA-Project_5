c61e68bb55ede9eb47bce00daca25866b082cb1c
blakkur8 <sigmarsnaer@gmail.com>
2022-10-21 13:23:37 +0000

    working on sphere

--- OpenGLCore/baseObjects.h
+++ OpenGLCore/baseObjects.h
@@ -5,7 +5,21 @@
 #ifndef PROJECT5_BASEOBJECTS_H
 #define PROJECT5_BASEOBJECTS_H
 #include "vector"
+#include "cmath"
 #include "GLFW/glfw3.h"
+
+class Sphere {
+public:
+    int slices;
+    int stacks;
+    int vertex_count;
+    std::vector<float> vertex_array;
+
+    void init(int stacks_inp, int slices_inp);
+    Sphere();
+    //void draw(Shader3D shader);
+    void draw();
+};
 
 class Cube {
 public:
