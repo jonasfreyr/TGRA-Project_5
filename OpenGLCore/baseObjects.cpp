4524b8efb4cd3331438ffec958f67e9b17ce1b16
blakkur8 <sigmarsnaer@gmail.com>
2022-10-21 13:41:01 +0000

    shader is now in sphere

--- OpenGLCore/baseObjects.cpp
+++ OpenGLCore/baseObjects.cpp
@@ -11,16 +11,32 @@ Sphere::Sphere() {
 
 }
 
-void Sphere::init(int stacks_inp, int slices_inp){
+void Sphere::init(int stacks_inp, int slices_inp) {
     stacks = stacks_inp;
     slices = slices_inp;
     vertex_count = 0;
     vertex_array = {};
 
-    float stack_interval = M_PI/ stacks;
-    float slice_interval = 2.0 * M_PI/ slices;
+    float stack_interval = M_PI / stacks;
+    float slice_interval = 2.0 * M_PI / slices;
 
+    float stack_angle;
+    float slice_angle;
+    for (int stack_count = 0; stack_count < stacks; stack_count++) {
+        stack_angle = stack_count * stack_interval;
+        for (int slice_count = 0; slice_count < slices + 1; slice_count++) {
+            slice_angle = slice_count * slice_interval;
+            vertex_array.push_back(sin(stack_angle) * cos(slice_angle));
+            vertex_array.push_back(cos(stack_angle));
+            vertex_array.push_back(sin(stack_angle) * sin(slice_angle));
 
+            vertex_array.push_back(sin(stack_angle + stack_interval) * cos(slice_angle));
+            vertex_array.push_back(cos(stack_angle + stack_interval));
+            vertex_array.push_back(sin(stack_angle + stack_interval) * sin(slice_angle));
+            vertex_count += 2;
+        }
+
+    }
 
 }
 
@@ -54,7 +70,7 @@ void Cube::init() {
             0.5, -0.5, -0.5,
             0.5, -0.5, 0.5,
             0.5, 0.5, 0.5,
-            0.5, 0.5, -0.5 };
+            0.5, 0.5, -0.5};
 
     normal_array = {
             0.0, 0.0, -1.0,
@@ -84,11 +100,13 @@ void Cube::init() {
     };
 }
 
-void Cube::draw(/*Shader3D shader*/){
-    /*
-     * shader.set_position_attribute(position_array)
-     * shader.set_normal_attribute(normal_array)
-     * */
+void Cube::draw(Shader3D shader) {
+    float *pos_array = &position_array[0];
+    float *norm_array = &normal_array[0];
+
+    shader.set_position_attribute(pos_array);
+    shader.set_normal_attribute(norm_array);
+
     glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
     glDrawArrays(GL_TRIANGLE_FAN, 4, 4);
     glDrawArrays(GL_TRIANGLE_FAN, 8, 4);
